70867c6a97e521e8b7cfa525cc004ee74e479928
chenxueliang <chen.xue.liang@fesco.com.cn>
2020-09-18 18:05:02 +0800

    80 consumer

---
+++ springcloud2020/cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
@@ -0,0 +1,16 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: chenxueliang
+ * @date: 2020/9/18 11:04
+ */
+@SpringBootApplication
+public class OrderMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMain80.class,args);
+    }
+}
