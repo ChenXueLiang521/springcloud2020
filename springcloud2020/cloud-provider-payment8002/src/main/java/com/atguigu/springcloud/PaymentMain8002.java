0df3c11a24417c3f1f928e1038eaa6e75a8e009a
chenxueliang <chen.xue.liang@fesco.com.cn>
2020-09-24 18:01:34 +0800

    jiqun

---
+++ springcloud2020/cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/PaymentMain8002.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @description:
+ * @author: chenxueliang
+ * @date: 2020/9/15 15:02
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class PaymentMain8002 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8002.class,args);
+    }
+}
