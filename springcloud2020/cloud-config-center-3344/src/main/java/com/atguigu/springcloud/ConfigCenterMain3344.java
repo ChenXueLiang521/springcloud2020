6c9b569cc19a36e10786b103ebcccda16af2d532
chenxueliang <chen.xue.liang@fesco.com.cn>
2020-11-24 11:37:38 +0800

    config center

---
+++ springcloud2020/cloud-config-center-3344/src/main/java/com/atguigu/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @description:
+ * @author: chenxueliang
+ * @date: 2020/11/24 9:25
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class,args);
+    }
+}
