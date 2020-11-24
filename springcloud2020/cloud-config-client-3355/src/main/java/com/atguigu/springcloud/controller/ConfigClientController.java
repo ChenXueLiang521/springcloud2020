f91d1f8f407bd0c4397756ab791c57e7dc4526f0
chenxueliang <chen.xue.liang@fesco.com.cn>
2020-11-24 18:04:29 +0800

    config center2

---
+++ springcloud2020/cloud-config-client-3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,26 @@
+package com.atguigu.springcloud.controller;
+
+import jdk.nashorn.internal.ir.annotations.Reference;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description:
+ * @author: chenxueliang
+ * @date: 2020/11/24 16:30
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+
+}
