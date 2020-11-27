6ab5af1353031e1d978cf481f85e5ab054420736
chenxueliang <chen.xue.liang@fesco.com.cn>
2020-11-27 17:51:28 +0800

    srpirng-bus

---
+++ springcloud2020/cloud-config-client-3366/src/main/java/com/atguigu/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @description:
+ * @author: chenxueliang
+ * @date: 2020/11/27 16:37
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class,args);
+    }
+}
